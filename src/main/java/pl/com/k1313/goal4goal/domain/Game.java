d576e19508ded094ed96e81f2d10a2ec09731733
kt1313 <tomaklim@wp.pl>
2021-09-17 04:58:07 +0200

    9 - 4.4 tworzenie listy zawodnikow , ktorzy bierze udzial w meczu

--- src/main/java/pl/com/k1313/goal4goal/domain/Game.java
+++ src/main/java/pl/com/k1313/goal4goal/domain/Game.java
@@ -4,27 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Component
 public class Game {
 
     @Autowired
-    @Qualifier(value = "player2")
-    Player player;
+//    @Qualifier(value = "player2")
+    List<Player> players;
 
     public Game() {
     }
 
     public void playGame() {
-        player.setAge(player.getAge() + 1);
+//        player.setAge(player.getAge() + 1);
 
     }
 
-    public void setPlayer(Player player) {
-        this.player = player;
+    public void setPlayer(List<Player> players) {
+        this.players = players;
     }
 
     @Override
     public String toString() {
-        return "Zawodnik " + player + " rozegrał mecz.";
+        return "W meczu zagrali: " + players.stream().map(Objects::toString).collect(Collectors.joining(","));
     }
 }
+
