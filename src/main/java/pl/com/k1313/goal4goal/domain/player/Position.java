f48fd5cc07e3d1d6e1c3bbd336fc0eaa49d7b09b
kt1313 <tomaklim@wp.pl>
2022-01-03 10:50:37 +0100

    cz1. przejscie na nowy kod, Player - mam juz odczytywanie zaznaczonych checkboxow i na tej podstawie pobiera playerID i listuje w konsoli oraz zaznacza firstsquadplayer na true (ale drewnianie, bo recznie) wypisuje tez tylko first11 do zrobienia: przypisanie poszczegolnych pozycji w odp miejsca w templatce, aby wygladalo jak sklad pilkarski

--- src/main/java/pl/com/k1313/goal4goal/domain/player/Position.java
+++ src/main/java/pl/com/k1313/goal4goal/domain/player/Position.java
@@ -5,25 +5,27 @@ import org.springframework.stereotype.Component;
 
 public enum Position {
 
-    GK ("Goalkeeper"),
-    RWB("Right Wingback"),
-    RCB("Right Centre-back"),
-    CB("Centre back"),
-    LCB("Left Centre-back"),
-    LWB("Left Wingback"),
-    RW("Right Winger"),
-    CMD("Centre Midfielder Defending"),
-    CM("Centre Midfielder"),
-    CMA("Centre Midfielder Attacking"),
-    LW("Left Winger"),
-    RF("Right Forward"),
-    CF("Centre Forward"),
-    LF("Left Forward");
+    GK ("Goalkeeper", "3"),
+    RWB("Right Wingback", "10"),
+    RCB("Right Centre-back", "9"),
+    CB("Centre back", "8"),
+    LCB("Left Centre-back", "7"),
+    LWB("Left Wingback", "6"),
+    RW("Right Winger", "15"),
+    CMD("Centre Midfielder Defending", "14"),
+    CM("Centre Midfielder", "13"),
+    CMA("Centre Midfielder Attacking", "12"),
+    LW("Left Winger", "11"),
+    RF("Right Forward", "19"),
+    CF("Centre Forward", "18"),
+    LF("Left Forward", "17");
 
     private final String position;
+    private final String number;
 
-    Position(String position){
-        this.position=position;
+
+    Position(String position, String number){
+        this.position=position; this.number=number;
     }
 
     public String toString(){
