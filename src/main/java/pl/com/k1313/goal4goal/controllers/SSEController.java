0b79d1a13d5d132042b2a8e554e6540568986a3e
kt1313 <tomaklim@wp.pl>
2022-03-21 12:31:33 +0100

    silnik meczowy cz10- działa, wpadają bramki, ale: - sciaga CALY komentarz w postaci pliku i wyswietla calosc, proby w Ajaxie... nie dziala - proba w sse . 1)stworzony plik css w osobnym folderze, 2)stworzony actions.js w osobnym i dzialaja OK
    TODO:
    - teraz na bazie DEMO sprobowac sse
    - odswiexaanie po kazdym pojedynczym komentarzu
    - nie tylko napastnik moze strzelic..(ale to pozniej)
    - dodac silnik gry
    -  testy dla MatchService

--- src/main/java/pl/com/k1313/goal4goal/controllers/SSEController.java
+++ src/main/java/pl/com/k1313/goal4goal/controllers/SSEController.java
@@ -1,11 +1,8 @@
 package pl.com.k1313.goal4goal.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.com.k1313.goal4goal.domain.star.StarService;
 
 
 @Controller
@@ -19,9 +16,15 @@ public class SSEController {
 //            this.starService = starService;
 //        }
 
+//    @GetMapping
+//    public String match(Model m) {
+//        return "ssetrial";
+//    }
+//
     @GetMapping
-    public String match(Model m) {
-        return "ssetrial";
+    public String getStyledPage() {
+//        model.addAttribute("name", "Baeldung Reader");
+        return "/styledPage";
     }
 }
 
